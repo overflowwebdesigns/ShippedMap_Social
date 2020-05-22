bf220b5c19e99ecc971e421bbefcae27aa8e084a
Justin Tharpe <justin@Justins-MacBook-Pro.local>
2020-05-22 00:11:53 -0400

    Test

--- ShippedMap_Social.php
+++ ShippedMap_Social.php
@@ -23,8 +23,8 @@ $orders = wc_get_orders(array());
 
 foreach($orders as $order){
     $states = get_post_meta($order->ID, '_billing_state', false);
-    var_dump($states);
-    echo $states;
+    $state = array_shift($states);
+    echo $state;
     echo "Line Break";
 }
 

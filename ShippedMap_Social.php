885634717f62c96f2af54ee0e6a6d316da4b5c22
Justin Tharpe <justin@Justins-MacBook-Pro.local>
2020-05-24 23:53:14 -0400

    Adding settings page

--- ShippedMap_Social.php
+++ ShippedMap_Social.php
@@ -10,6 +10,71 @@
 */
 
 
+if (!defined('ABSPATH')) die('No direct access allowed');
+
+function create_plugin_settings_page()
+{
+
+    // Add the menu item and page
+    $page_title = 'Shipped Map Social';
+    $menu_title = 'ShippedMap Social';
+    $capability = 'manage_options';
+    $slug = 'shipped_map_social';
+    $callback = 'plugin_settings_page_content';
+    $icon = 'dashicons-admin-plugins';
+    $position = 100;   
+    add_menu_page($page_title, $menu_title, $capability, $slug, $callback, $icon, $position);
+    }
+
+function plugin_settings_page_content(){
+    global $content;
+    global $wpdb;
+
+    ?><center><h1>Shipped Map Social Settings</h1></center><?php
+
+    ?>
+    <form method="post" action="options.php"> <?php
+    settings_fields( "header_section" );
+    do_settings_sections( "shipped_map_social" );
+    submit_button();
+    ?></form><?php
+
+}
+
+
+function jt_wrf_display_options(){
+
+    add_settings_section( 'header_section', 'Product Selection Settings', 'jt_header_func', 'shipped_map_social' );
+    add_settings_field( 'jt_num_to_keep', 'How Many Prodcuts to Keep', 'jt_num_to_keep', 'shipped_map_social', 'header_section' );
+    add_settings_field( 'jt_when_to_change', 'How Often Should Featured Products Change (In Seconds)', 'jt_when_to_change', 'shipped_map_social', 'header_section' );
+    register_setting( 'header_section', 'jt_num_to_keep' );
+    register_setting( 'header_section', 'jt_when_to_change' );
+    
+}
+
+function jt_header_func(){echo "This will configured various options associated with the plugin.";}
+
+function jt_num_to_keep(){
+
+    ?>
+    <input type="number" name="jt_num_to_keep" id="jt_num_to_keep" value="<?php echo get_option( 'jt_num_to_keep' ); ?>" />
+    <?php
+}
+
+function jt_when_to_change(){
+
+    ?>
+    <input type="number" name="jt_when_to_change" id="jt_when_to_change" value="<?php echo get_option( 'jt_when_to_change' ); ?>" />
+    <?php
+}
+
+function reconfigure_options(){
+    RunFeatured();
+}
+
+
+
+
 function owd_load_plugin_css() {
     $plugin_url = plugin_dir_url( __FILE__ );
 

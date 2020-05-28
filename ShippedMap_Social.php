13a912ceff192473447ac924af2fa0ac2e89fd6f
Justin Tharpe <justin@Justins-MacBook-Pro.local>
2020-05-27 22:36:07 -0400

    Test

--- ShippedMap_Social.php
+++ ShippedMap_Social.php
@@ -96,7 +96,7 @@ $orders = wc_get_orders(array()); //Gets all orders.
     
     $('#map').usmap({ //Creates interactive JS map
         click: function(event, data) {
-            console.log('You clicked '+data.name),
+            console.log('You clicked '+data.name)
         };
         showLabels: true,
         stateStyles: {fill: <?php echo "'" . get_option('jt_color_states') . "'"; ?>}, //defines the default color for a state. In our case the color of a state we have not shipped products to.

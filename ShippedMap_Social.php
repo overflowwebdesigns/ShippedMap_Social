c10707812c8ceabad299462c18ffab3d6faae6c0
Justin Tharpe <justin@Justins-MacBook-Pro.local>
2020-06-04 23:48:14 -0400

    Test

--- ShippedMap_Social.php
+++ ShippedMap_Social.php
@@ -167,7 +167,7 @@ $orders = wc_get_orders(array()); //Gets all orders.
     </script>
     </div>
     <div id="openModal" class="modalDialog" style="display: none;">
-    <div>
+    <div class="modal-content">
         <a href="#close" title="Close" class="close" onclick="$('#openModal').hide()">X</a>
         <h2>Modal Box</h2>
         <p>Hello world</p>

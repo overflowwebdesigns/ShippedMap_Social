13aa4ecc7ac68893b4d37cb0c2ba7b48a2f95ef2
Justin Tharpe <justin@Justins-MacBook-Pro.local>
2020-06-09 12:22:06 -0400

    Test

---
+++ map.php
@@ -0,0 +1,112 @@
+<?php
+
+
+function owd_map_shortcode($atts) {
+
+$orders = wc_get_orders(array()); //Gets all orders.
+
+   ?>
+    <html>
+    <head>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"> </script> 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/us-map/1.0.1/jquery.usmap.js"></script>
+    <link rel="stylesheet" href="https://unpkg.com/swiper/css/swiper.css">
+    <link rel="stylesheet" href="https://unpkg.com/swiper/css/swiper.min.css">
+    </head>
+    <center>
+    <h2 class="elementor-heading-title elementor-size-default">Who is wearing Loud Proud American?</h2>
+    <h3 class="elementor-heading-title elementor-size-default">Help us cover the USA!</h3>
+    <div id="map" style="width: auto; height: 800px;">
+    <script>
+
+    $('#map').usmap({ //Creates interactive JS map
+        showLabels: true,
+        stateStyles: {fill: <?php echo "'" . get_option('jt_color_states') . "'"; ?>}, //defines the default color for a state. In our case the color of a state we have not shipped products to.
+        stateSpecificStyles: {
+            <?php
+            foreach($orders as $order){
+                $states = get_post_meta($order->ID, '_billing_state', false); //Takes the order ID's and pulls the state the order is from.
+                $state = array_shift($states);
+                echo "'" . $state . "': {fill: " . "'" . get_option('jt_shipped_state_color') . "'" . "},"; //Defines the color to a state we have shipped products to.
+            } ?>
+        },
+        click: function(event, data) {
+            var state = data.name;
+            var i;
+            var slides = "";
+                //console.log('You clicked '+state); //This is going to be a pop up lightbox style image slider that opens to show images of people from those states wearing the products.
+                $.ajax({
+                type:'POST',
+                url: '../../wp-admin/admin-ajax.php',
+                data: {
+                    action: 'jt_state_images',
+                    state: state
+                },
+                dataType: "json",
+                success:function (output) {
+                    for (i = 0; i < output.length; i++) {
+                        slides += '<div class="swiper-slide"><img src=' + output[i] + '></div><br>';
+                    }
+                    $('#openModal h2').html(state);
+                    $('#swiper').html(slides);
+                    $('#openModal').show();
+                },
+                error:function (error) {
+                    
+                }
+
+            });
+        }
+    });
+    
+    </script>
+    </div>
+    <body>
+    <div id="openModal" class="modalDialog" style="display: none;">
+    <div class="modal-content">
+        <a href="#close" title="Close" class="close" onclick="$('#openModal').hide()">X</a>
+        <h2>Modal Box</h2>
+        <p>Hello world</p>
+        <script src="https://unpkg.com/swiper/js/swiper.min.js"></script>
+        <link rel="stylesheet" href="https://unpkg.com/swiper/css/swiper.css">
+        <link rel="stylesheet" href="https://unpkg.com/swiper/css/swiper.min.css">
+        <!-- Slider main container -->
+        <div class="swiper-container">
+            <!-- Additional required wrapper -->
+            <div id="swiper" class="swiper-wrapper">
+                <!-- Slides -->
+                
+            </div>
+            <!-- If we need pagination -->
+            <div class="swiper-pagination"></div>
+
+            <!-- If we need navigation buttons -->
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+
+            <!-- If we need scrollbar -->
+            <div class="swiper-scrollbar"></div>
+        </div>
+        <script>
+        var mySwiper = new Swiper ('.swiper-container', {
+            // Optional parameters
+            direction: 'horizontal',
+            loop: true,
+            centeredSlides: true,
+            slidesPerView: 1,
+
+            pagination: '.swiper-pagination',
+            paginationClickable: '.swiper-pagination',
+            nextButton: '.swiper-button-next',
+            prevButton: '.swiper-button-prev',
+        })
+        </script>
+    </div>
+    </div>
+    </center>
+    </body>
+    </html>
+
+    <?php
+
+}

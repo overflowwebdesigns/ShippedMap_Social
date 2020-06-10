beff7197c0bc06056e9925ca6eb6b17a2cbb077c
Justin Tharpe <justin@Justins-MacBook-Pro.local>
2020-06-09 21:40:08 -0400

    Test

--- map.php
+++ map.php
@@ -88,18 +88,22 @@ $orders = wc_get_orders(array()); //Gets all orders.
             <div class="swiper-scrollbar"></div>
         </div>
         <script>
-        $(document).ready(function () {
-        //initialize swiper when document ready
-            var mySwiper = new Swiper ('.swiper-container', {
-                // Optional parameters
-                direction: 'horizontal',
-                loop: true
-                navigation: {
-                    nextEl: '.swiper-button-next',
-                    prevEl: '.swiper-button-prev',
-                }
-            })
-        });
+        var mySwiper = new Swiper ('.swiper-container', {
+            // Optional parameters
+            direction: 'horizontal',
+            loop: true,
+            centeredSlides: true,
+            slidesPerView: 1,
+
+            pagination: {
+            el: '.swiper-pagination',
+            },
+            navigation: {
+            	nextEl: '.swiper-button-next',
+            	prevEl: '.swiper-button-prev',
+            }
+            
+        })
         </script>
     </div>
     </div>

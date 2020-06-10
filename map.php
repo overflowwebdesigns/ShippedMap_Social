210167313a9fa3d3030b5f6a2d114c166e3167c0
Justin Tharpe <justin@Justins-MacBook-Pro.local>
2020-06-09 22:23:19 -0400

    Test

--- map.php
+++ map.php
@@ -88,12 +88,11 @@ $orders = wc_get_orders(array()); //Gets all orders.
         var mySwiper = new Swiper ('.swiper-container', {
             // Optional parameters
             direction: 'horizontal',
-            loop: true,
-            centeredSlides: true,
+            loop:true,
+            centeredSlides:true,
             slidesPerView: 1,
             observer: true,
             observeParents: true,
-            initialSlide: 0, //this one accept a number according to docs
 
             pagination: {
             el: '.swiper-pagination',

95761d6f22a9261dc3c9754d76a4e5ff67a6891c
Justin Tharpe <justin@Justins-MacBook-Pro.local>
2020-06-11 22:18:08 -0400

    Test

--- map.php
+++ map.php
@@ -98,7 +98,7 @@ $orders = wc_get_orders(array()); //Gets all orders.
         for (i = 0; i < x.length; i++) {
             x[i].style.display = "none";  
         }
-        x[slideIndex-1].style.display = "block";  
+        
         }
         </script>
     </div>

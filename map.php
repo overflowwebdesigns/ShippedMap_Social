257aa86eed75b4deee7aa395553b214c23051d06
Justin Tharpe <justin@Justins-MacBook-Pro.local>
2020-06-11 22:10:20 -0400

    Test

--- map.php
+++ map.php
@@ -77,10 +77,6 @@ $orders = wc_get_orders(array()); //Gets all orders.
         <h2 class="w3-center">Manual Slideshow</h2>
 
         <div id="slider" class="w3-content w3-display-container">
-        <img class="mySlides" src="img_snowtops.jpg" style="width:100%">
-        <img class="mySlides" src="img_lights.jpg" style="width:100%">
-        <img class="mySlides" src="img_mountains.jpg" style="width:100%">
-        <img class="mySlides" src="img_forest.jpg" style="width:100%">
 
         <button class="w3-button w3-black w3-display-left" onclick="plusDivs(-1)">&#10094;</button>
         <button class="w3-button w3-black w3-display-right" onclick="plusDivs(1)">&#10095;</button>

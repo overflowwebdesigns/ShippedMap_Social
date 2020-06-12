f53090d6d9f6be0f5be7016cbd460a00a9df614e
Justin Tharpe <justin@Justins-MacBook-Pro.local>
2020-06-11 22:15:38 -0400

    Test

--- map.php
+++ map.php
@@ -50,7 +50,7 @@ $orders = wc_get_orders(array()); //Gets all orders.
                 dataType: "json",
                 success:function (output) {
                     for (i = 0; i < output.length; i++) {
-                        slides += '<img class="mySlides" src=' + output[i] + 'style="width:100%">';
+                        slides += '<img class="mySlides" src="' + output[i] + '"style="width:100%">';
                     }
                     $('#openModal h2').html(state);
                     $('#slider').html(slides);
